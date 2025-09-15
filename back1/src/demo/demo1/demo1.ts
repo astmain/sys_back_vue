051563def0328cf89ad68af1947284339302a8c3
astmain <1311192345@qq.com>
2025-09-15 23:26:49 +0800

    1

--- back1/src/demo/demo1/demo1.ts
+++ back1/src/demo/demo1/demo1.ts
@@ -4,6 +4,13 @@ import { /*api开发*/ Api_public } from '@src/App_Auth'
 import { /*数据库*/ db } from '@src/App_Prisma'
 import { /*控制器装饰器*/ Api_Controller } from '@src/Plugins/Api_Controller'
 import { /*控制器装饰器*/ Api_Post } from '@src/Plugins/Api_Post'
+import { tb_test1 } from '@prisma/client'
+
+
+
+
+
+
 
 
 
@@ -11,8 +18,9 @@ import { /*控制器装饰器*/ Api_Post } from '@src/Plugins/Api_Post'
 @Api_public()
 @Api_Controller('demo1测试')
 export class demo1 {
-  @Api_Post('查找-示例1-list', '')
+  @Api_Post('查找-示例1-list', '', )
   async find_list_demo1(@Body() body: any) {
+ 
     const list = await db.tb_test1.findMany({ where: { password: '123456' } })
     return list
   }
