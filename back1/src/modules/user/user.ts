39761b8a158e78b6df967539ea548185d1aeb857
astmain <1311192345@qq.com>
2025-09-16 19:16:58 +0800

    1

--- back1/src/modules/user/user.ts
+++ back1/src/modules/user/user.ts
@@ -12,20 +12,19 @@ import { /*dto*/ find_one_user } from './dto/find_one_user'
 
 @Api_Controller('用户管理')
 export class user {
-  @Api_Post('查询-用户-列表', '', tb_user)
+  @Api_Post('查询-用户-列表')
   async find_list_user(@Body() body: find_list_user) {
     let where: any = { ...body }
     where.phone = { contains: body.phone || '' }
     where.name = { contains: body.name || '' }
     let res = await db_page_find_many(db.tb_user, { where: where, orderBy: { created_at: 'desc' } })
     console.log(`111---222:`, res)
-    return { code: 200, msg: '成功:查询用户列表成功', result: res }
+    return { code: 200, msg: '成功:查询-用户-列表', result: res }
   }
 
-  @Api_Post('查询-用户-详情', '', tb_user)
+  @Api_Post('查询-用户-详情')
   async find_one_user(@Body() body: find_one_user) {
     let one = await db.tb_user.findUnique({ where: body })
-    // if (!one) return { code: 400, msg: '失败:查询-用户-详情', result: null }
     return { code: 200, msg: '成功:查询-用户-详情', result: one }
   }
 }
