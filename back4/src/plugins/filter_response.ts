0361b984b8105ac0d4e73cd85a63cf928cb482b3
astmain <1311192345@qq.com>
2025-10-01 01:45:59 +0800

    1

--- back4/src/plugins/filter_response.ts
+++ back4/src/plugins/filter_response.ts
@@ -1,7 +1,8 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
-import dayjs from 'dayjs'
+// import dayjs from 'dayjs'
+import dayjs = require('dayjs')
 
 export interface Response<T> {
   code: number
