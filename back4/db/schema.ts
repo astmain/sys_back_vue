4a644aa3da2d3155e1d0b0f3f0dc810759912382
astmain <1311192345@qq.com>
2025-09-30 23:38:34 +0800

    1

--- back4/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
-
-export const tb_test1 = sqliteTable('tb_test1', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name').notNull(),
-  created_at: text('created_at').notNull().$defaultFn(() => new Date().toISOString()),
-  updated_at: text('updated_at').notNull().$defaultFn(() => new Date().toISOString()),
-})
