d525faa512e88472d3db2524c215f7f0002b7b65
astmain <1311192345@qq.com>
2025-10-07 17:24:40 +0800

    1

--- vuejs6/src/view_admin/home/home.vue
+++ vuejs6/src/view_admin/home/home.vue
@@ -1,4 +1,18 @@
-<template></template>
+<template>
+  <div>
+    <el-table :data="tree_menu" style="width: 100%">
+    <el-table-column prop="id" label="ID" />
+    <el-table-column prop="name" label="名称" />
+    <el-table-column prop="path" label="路径" />
+    <el-table-column prop="remark" label="备注" />
+    <el-table-column prop="parent_id" label="父ID" />
+    <el-table-column type="expand"> </el-table-column>
+  </el-table>
+
+  <!-- <pre>{{ tree_menu }}</pre> -->
+  </div>
+
+</template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue"
@@ -6,17 +20,17 @@ import { ElMessage } from "element-plus"
 // 基础数据
 let tree_menu = ref([
   {
-    id: "订单管理",
+    id: "menu_1",
     name: "首页",
     path: "/home",
     remark: null,
     parent_id: null,
     is_all: false,
-    permiss: [
-      { id: "home_1", name: "查看", remark: "" },
-      { id: "home_2", name: "删除", remark: "" },
+    type: "menu",
+    children: [
+      { id: "home_1", type: "button", name: "查看", remark: "" },
+      { id: "home_2", type: "button", name: "删除", remark: "" },
     ],
-    children: [],
   },
   {
     id: "menu_2",
@@ -25,6 +39,7 @@ let tree_menu = ref([
     remark: null,
     parent_id: null,
     is_all: false,
+    type: "menu",
     children: [
       {
         id: "sub_2001",
@@ -33,11 +48,11 @@ let tree_menu = ref([
         remark: null,
         parent_id: "menu_2",
         is_all: false,
-        permiss: [
-          { id: "order_1", name: "查看", remark: "" },
-          { id: "order_2", name: "删除", remark: "" },
+        type: "menu",
+        children: [
+          { id: "order_1", type: "button", name: "查看", remark: "" },
+          { id: "order_2", type: "button", name: "删除", remark: "" },
         ],
-        children: [],
       },
       {
         id: "sub_2002",
@@ -46,11 +61,11 @@ let tree_menu = ref([
         remark: null,
         parent_id: "menu_2",
         is_all: false,
-        permiss: [
-          { id: "product_1", name: "查看", remark: "" },
-          { id: "product_1", name: "删除", remark: "" },
+        type: "menu",
+        children: [
+          { id: "product_1", type: "button", name: "查看", remark: "" },
+          { id: "product_2", type: "button", name: "删除", remark: "" },
         ],
-        children: [],
       },
     ],
   },
--- vuejs6/src/view_admin/home/home原版.vue
+++ vuejs6/src/view_admin/home/home原版.vue
@@ -1,4 +1,9 @@
-<template></template>
+<template>
+
+
+
+  <pre>{{ tree_menu }}</pre>
+</template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue"
@@ -6,17 +11,17 @@ import { ElMessage } from "element-plus"
 // 基础数据
 let tree_menu = ref([
   {
-    id: "订单管理",
+    id: "menu_1",
     name: "首页",
     path: "/home",
     remark: null,
     parent_id: null,
     is_all: false,
-    permiss: [
-      { id: "home_1", name: "查看", remark: "" },
-      { id: "home_2", name: "删除", remark: "" },
+    type: "menu",
+    children: [
+      { id: "home_1", type: "button", name: "查看", remark: "" },
+      { id: "home_2", type: "button", name: "删除", remark: "" },
     ],
-    children: [],
   },
   {
     id: "menu_2",
@@ -25,6 +30,7 @@ let tree_menu = ref([
     remark: null,
     parent_id: null,
     is_all: false,
+    type: "menu",
     children: [
       {
         id: "sub_2001",
@@ -33,11 +39,11 @@ let tree_menu = ref([
         remark: null,
         parent_id: "menu_2",
         is_all: false,
-        permiss: [
-          { id: "order_1", name: "查看", remark: "" },
-          { id: "order_2", name: "删除", remark: "" },
+        type: "menu",
+        children: [
+          { id: "order_1", type: "button", name: "查看", remark: "" },
+          { id: "order_2", type: "button", name: "删除", remark: "" },
         ],
-        children: [],
       },
       {
         id: "sub_2002",
@@ -46,11 +52,11 @@ let tree_menu = ref([
         remark: null,
         parent_id: "menu_2",
         is_all: false,
-        permiss: [
-          { id: "product_1", name: "查看", remark: "" },
-          { id: "product_1", name: "删除", remark: "" },
+        type: "menu",
+        children: [
+          { id: "product_1", type: "button", name: "查看", remark: "" },
+          { id: "product_2", type: "button", name: "删除", remark: "" },
         ],
-        children: [],
       },
     ],
   },
