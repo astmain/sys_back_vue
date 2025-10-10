4b44dad73766a3968d5b3f34c77d6c6e6cfe082f
astmain <1311192345@qq.com>
2025-10-10 10:53:21 +0800

    1

--- vuejs6/src/view_admin/home/home.vue
+++ vuejs6/src/view_admin/home/home.vue
@@ -1,16 +1,29 @@
 <template>
-  <el-button @click="handle_click">点击:{{ count }}</el-button>
+  <el-button @click="handle_click">点击</el-button>
 </template>
 
 <script setup lang="tsx">
 import { ref } from "vue"
 import { ElButton } from "element-plus"
 
-let count = $ref(1)
+let list_img = $ref([
+  {
+    url: "https://server.oss.yun3d.com/oss_api/static_stream?path_static=/public/3/粉牡丹_2025-09_16_08_43_29_240645_new.png",
+    name: "粉牡丹.png",
+    size: 80241,
+    size_format: "78.36 KB",
+  },
+  {
+    url: "https://server.oss.yun3d.com/oss_api/static_stream?path_static=/public/3/黄牡丹_2025-09_17_03_03_54_704278_new.png",
+    name: "黄牡丹.png",
+    size: 93921,
+    size_format: "91.72 KB",
+  },
+])
 
 const handle_click = () => {
-  console.log("点击")
-  count++
+  console.log("list_img", list_img)
+  //我像点击按钮后下载 list_img中的url
 }
 </script>
 <style></style>
