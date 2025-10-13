6db0fa22cb5b7c36a09bab55e598ee072f8a120c
astmain <1311192345@qq.com>
2025-10-13 15:22:53 +0800

    1

--- vuejs6/src/view_shop/print_3d/print_3d.vue
+++ vuejs6/src/view_shop/print_3d/print_3d.vue
@@ -11,15 +11,29 @@ import { BUS } from "@/BUS"
 import { ref } from "vue"
 // @ts-ignore
 import { canvas_three_parse } from "./canvas_three_parse.js"
+import { tool_upload } from "./tool_upload.ts"
 
 async function file_area_click(event: any) {
   ;(document?.querySelector(".file_input") as HTMLInputElement)?.click()
 }
 
 async function on_change_file(event: any) {
-  let result = await canvas_three_parse({ canvas: document.querySelector(".canvas_three_parse"), file: event.target.files[0] })
-  console.log(`result:`, result)
-  event.target.value = "" // 清空input的值
+  try {
+    const file = event.target.files[0]
+    // 绘制three解析
+    const result = await canvas_three_parse({ canvas: document.querySelector(".canvas_three_parse"), file })
+    console.log(`result:`, result)
+
+    // 上传文件
+    await tool_upload({ file, path_static: "/user/1", callback })
+    function callback(res: any) {
+      console.log(`result_upload:`, res)
+    }
+  } catch (error) {
+    console.log(`on_change_file---error:`, error)
+  } finally {
+    event.target.value = "" // 清空input的值
+  }
 }
 </script>
 
