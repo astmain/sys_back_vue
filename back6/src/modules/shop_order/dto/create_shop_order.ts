29c8cfb15fe0d033642e3351b1cdf88b1a216ca7
astmain <1311192345@qq.com>
2025-10-22 00:53:46 +0800

    1

---
+++ back6/src/modules/shop_order/dto/create_shop_order.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsArray, IsIn, IsOptional, IsNumber, IsNotEmpty, IsString } from 'class-validator'
+
+export class create_shop_order {
+  @ApiProperty({ description: '订单id', example: 'cuid_string' })
+  @IsString({ message: '订单id-必须是字符串' })
+  @IsNotEmpty({ message: '订单id-必须不能为空' })
+  @IsOptional()
+  order_id?: string
+
+  @ApiProperty({ description: '用户id', example: 'user_1' })
+  @IsString({ message: '用户id-必须是字符串' })
+  @IsNotEmpty({ message: '用户id-必须不能为空' })
+  user_id: string
+}
---
+++ back6/src/modules/shop_order/dto/find_list_shop_order.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsArray, IsIn, IsOptional, IsNumber, IsNotEmpty, IsString } from 'class-validator'
+
+export class find_list_shop_order {
+  @ApiProperty({ description: '用户id', example: 'user_1' })
+  @IsString({ message: '用户id-必须是字符串' })
+  @IsNotEmpty({ message: '用户id-必须不能为空' })
+  user_id: string
+}
