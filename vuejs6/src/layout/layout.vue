124bb666429dc4a323612a25d4fe5e85cf091226
astmain <1311192345@qq.com>
2025-10-21 17:17:13 +0800

    1

--- vuejs6/src/layout/layout.vue
+++ vuejs6/src/layout/layout.vue
@@ -10,6 +10,10 @@
         <el-menu style="flex: 1" v-show="BUS.web_type === 'shop'" mode="horizontal" :default-active="active_menu" router class="header_menu" color background-color="#304156" text-color="#bfcbd9" active-text-color="#409eff">
           <el-menu-item index="/print_3d">3D打印</el-menu-item>
           <el-menu-item index="/model_shop">3D模型</el-menu-item>
+          <div class="ml-auto cursor-pointer flex-center flex gap-2" style="z-index: 1000" @click="() => router.push('/cart')">
+            <span class="icon-cart !bg-red-500" />
+            购物车
+          </div>
         </el-menu>
 
         <div style="display: flex; align-items: center; gap: 10px; justify-content: center">
