28dff4fab4f9878874918d0b896a07a13c0ce0e7
astmain <1311192345@qq.com>
2025-10-28 21:57:11 +0800

    1

--- vuejs6/src/view_shop/print_3d/com_dialog_print_product.vue
+++ vuejs6/src/view_shop/print_3d/com_dialog_print_product.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="print_3d">
     <el-dialog v-model="show" title="修改" width="800px" height="500px" :close-on-click-modal="false" draggable>
-      <div class="flex-col gap-2">
+      <div class="flex-col gap-8">
         <h1 class="uno_prefix1 text-18px">选择材料</h1>
         <el-radio-group class="radio111" v-model="radio_material">
-          <el-radio border v-for="item in material_list" :key="item.name" :label="item.name">{{ item.name }}</el-radio>
+          <el-radio v-for="item in material_list" :key="item.name" :label="item.name" border>{{ item.name }}</el-radio>
         </el-radio-group>
 
         <el-table :data="table_data" style="width: 100%">
           <el-table-column width="55">
             <template #default="scope">
-              <div @click="handle_radio_click(scope.row)" v-if="radio_row == scope.row" class="icon-check-ok !bg-blue-500 !bg-blue-700"></div>
+              <div @click="handle_radio_click(scope.row)" v-if="form_data.arg_material == scope.row" class="icon-check-ok !bg-blue-500 !bg-blue-700"></div>
               <div @click="handle_radio_click(scope.row)" v-else class="icon-check-no !bg-gray-200"></div>
             </template>
           </el-table-column>
@@ -34,7 +34,7 @@
             <Cradio_item class="w-70px" value="需要"> 需要</Cradio_item>
           </Cradio>
           <com_dialog_nut ref="ref_com_dialog_nut" />
-          <el-select v-if="nut_radio == '需要'" v-model="form_data.arg_nut" multiple value-key="value" placeholder="请选择螺母" @visible-change="show_aaaa">
+          <el-select v-if="nut_radio == '需要'" v-model="form_data.arg_nut" multiple value-key="value" placeholder="请选择螺母" @visible-change="handle_open_com_dialog_nut">
             <template #label="obj">
               <span>{{ obj.value.name }} </span>
             </template>
@@ -71,7 +71,6 @@ const form = ref<info_print_card>({} as info_print_card)
 const group_arg_print_material = ref({} as any)
 const material_list = ref<any[]>([])
 const radio_material = ref<string>("")
-const radio_row = ref<any>(null)
 
 const nut_radio = ref("需要")
 const nut_select_options = ref<any[]>([])
@@ -97,8 +96,9 @@ function open(arg: any, a_group_arg_print_material: any, a_material_list: any) {
   material_list.value = a_material_list
   console.log("open---radio_material", radio_material.value)
   radio_material.value = material_list.value[0].name
-  radio_row.value = table_data.value[0]
-  form_data.value.arg_polish = group_arg_print_material.value["打磨"][0]
+  form_data.value.arg_material = table_data.value[0] //参数材料
+  form_data.value.arg_nut = [group_arg_print_material.value["螺母"][0]] //参数螺母
+  form_data.value.arg_polish = group_arg_print_material.value["打磨"][0] //参数打磨
   show.value = true
 }
 function close() {
@@ -111,18 +111,9 @@ function submit() {
 
 function handle_radio_click(row: any) {
   form_data.value.arg_material = row
-  radio_row.value = row
 }
 
-function save_nut() {
-  console.log("save_nut---group_arg_print_material", group_arg_print_material.value["螺母"])
-  let list = group_arg_print_material.value["螺母"].filter((item: any) => item.count > 0)
-  console.log("save_nut---list", list)
-  form_data.value.arg_nut = list
-  nut_show.value = false
-}
-
-function show_aaaa(visible: boolean) {
+function handle_open_com_dialog_nut(visible: boolean) {
   if (visible) {
     ref_com_dialog_nut.value.open({ list_nut: group_arg_print_material.value["螺母"] })
     ref_com_dialog_nut.value.callback = (res: any) => {
